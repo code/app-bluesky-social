7324ba5149d15a6835847e700cacf578bbb184c2
Hailey <me@haileyok.com>
2024-07-09 17:10:57 -0700

    disabled state for btn

--- src/view/com/composer/Composer.tsx
+++ src/view/com/composer/Composer.tsx
@@ -524,6 +524,23 @@ export const ComposePost = observer(function ComposePost({
                       start={{x: 0, y: 0}}
                       end={{x: 1, y: 1}}
                       style={styles.postBtn}>
+                      {/* @TODO video make something better than this */}
+                      {videoUploadState.status !== 'idle' && publishOnUpload ? (
+                        <View
+                          style={[
+                            {
+                              position: 'absolute',
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              bottom: 0,
+                              backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                              zIndex: 1,
+                              borderRadius: 100,
+                            },
+                          ]}
+                        />
+                      ) : null}
                       <Text style={[s.white, s.f16, s.bold]}>
                         {replyTo ? (
                           <Trans context="action">Reply</Trans>
